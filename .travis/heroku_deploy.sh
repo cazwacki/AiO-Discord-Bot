313e7c28a1e074096ae39a145c8c887ca048d225
cazwacki <cazwacki@vt.edu>
2020-08-03 14:49:02 -0400

    Moved deployment to .travis.yml

--- .travis/heroku_deploy.sh
+++
@@ -1,4 +0,0 @@
-wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-heroku container:login
-heroku container:push web --app $HEROKU_APP_NAME
-heroku container:release web --app $HEROKU_APP_NAME
