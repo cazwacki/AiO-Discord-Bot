40854cfcb1a8814847d462d58bcdff5845b47452
cazwacki <cazwacki@vt.edu>
2020-08-04 09:40:22 -0400

    Set up bot to ignore debug channel in production

--- bot.go
+++ bot.go
@@ -11,8 +11,12 @@ import (
 )
 
 var start time.Time
+var prod_mode bool
 
 func Run_bot(token string) {
+	if os.Getenv("PROD_MODE") == "true" {
+		prod_mode = true
+	}
 	start = time.Now()
 
 	dg, err := discordgo.New("Bot " + token)
@@ -40,7 +44,10 @@ func Run_bot(token string) {
 }
 
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-
+	// Ignore my testing channel
+	if prod_mode && m.ChannelID == "739852388264968243" {
+		return
+	}
 	// Ignore all messages created by the bot itself
 	if m.Author.ID == s.State.User.ID {
 		return
