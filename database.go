41dbc3dbdb3c706b5be0617dbe28fdf1334bffe0
cazwacki <cazwacki@vt.edu>
2022-05-14 19:19:41 -0400

    Fix twitter behavior

--- database.go
+++ database.go
@@ -924,6 +924,7 @@ autoshrine channel.
 */
 func handleTweet(s *discordgo.Session, v twitter.Tweet) {
 	logInfo("Handling a tweet")
+	fmt.Println(v.User.Name)
 	if v.User.ID != 4850837842 || !strings.HasPrefix(v.FullText, "This week's shrine is") {
 		logInfo("Tweet not a shrine from DeadByBHVR. Skipping...")
 		return
